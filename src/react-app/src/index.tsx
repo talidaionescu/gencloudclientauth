9b6f75a1f8990fc33377d65e26ddc70d34e98a3a
Zachary <zach.servies@genesys.com>
2021-06-02 08:33:28 -0400

    test

--- src/react-app/src/index.tsx
+++ src/react-app/src/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { registerElements } from './configs/registerGUXComponents';
+import './configs/i18n';
+
+registerElements();
+console.log('public url: ', process.env.PUBLIC_URL);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<div>Loading i18n</div>}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 );
