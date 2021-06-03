0427896ce086bd1eef57afb62fee0f5c8ab7701d
Zachary <zach.servies@genesys.com>
2021-06-03 10:54:33 -0400

    env var test

--- src/react-app/src/configs/i18n.ts
+++ src/react-app/src/configs/i18n.ts
@@ -4,9 +4,10 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 const backendOptions = {
-  loadPath: `https://dhqbrvplips7x.cloudfront.net/client-auth/PCM-1645---Translations/Config-2/${process.env.REACT_APP_PUBLIC_URL}i18n/{{lng}}.json`,
-  // loadPath: `${process.env.PUBLIC_URL}i18n/{{lng}}.json`
+  // loadPath: `https://dhqbrvplips7x.cloudfront.net/client-auth/PCM-1645---Translations/Config-2/${process.env.REACT_APP_PUBLIC_URL}i18n/{{lng}}.json`,
+  loadPath: `${process.env.PUBLIC_URL}i18n/{{lng}}.json`
 };
+console.log('another test: ', process.env.PUBLIC_URL);
 i18n
   .use(Backend)
   .use(LanguageDetector)
