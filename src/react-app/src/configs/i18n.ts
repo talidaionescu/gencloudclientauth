c7f1a97293dc664e405f01db15270eefcc7a52bc
Zachary <zach.servies@genesys.com>
2021-06-03 11:01:32 -0400

    another one

--- src/react-app/src/configs/i18n.ts
+++ src/react-app/src/configs/i18n.ts
@@ -5,7 +5,7 @@ import { initReactI18next } from 'react-i18next';
 
 const backendOptions = {
   // loadPath: `https://dhqbrvplips7x.cloudfront.net/client-auth/PCM-1645---Translations/Config-2/${process.env.REACT_APP_PUBLIC_URL}i18n/{{lng}}.json`,
-  loadPath: `${process.env.PUBLIC_URL}i18n/{{lng}}.json`
+  loadPath: `${process.env.PUBLIC_URL}/i18n/{{lng}}.json`
 };
 console.log('another test: ', process.env.PUBLIC_URL);
 i18n
